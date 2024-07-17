2e7c0eb6a4246aa6eb632eba0b71ff63d109e1b9
Vladislav-Ciuperca <ciuperca37@gmail.com>
2024-07-17 16:56:37 +0200

    creo model -rms di tecnology

---
+++ app/Http/Controllers/TecnologyController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\tecnology;
+use Illuminate\Http\Request;
+
+class TecnologyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(tecnology $tecnology)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(tecnology $tecnology)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, tecnology $tecnology)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(tecnology $tecnology)
+    {
+        //
+    }
+}
